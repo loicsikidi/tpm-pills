ef2fb46c36b27e61bc78e4749adb5ab87d353bee
Loïc Sikidi <11026721+loicsikidi@users.noreply.github.com>
2025-05-30 19:31:08 +0200

    feat: ✨ migrate to mdbook-sitemap-gen as plugin (#26)
    * feat: ✨ migrate to mdbook-sitemap-gen as plugin
    * chore(nix-shell): 🚀 bump mdbook to v0.4.50
    * cicd: 👷 try to fix nix error w/ darwin

--- nix/pkgs/mdbook-sitemap-generator.nix
+++ nix/pkgs/mdbook-sitemap-generator.nix
@@ -1,16 +1,16 @@
-{ fetchFromGitHub, buildGoModule, }:
+{ fetchFromGitHub, buildGoModule }:
 
 buildGoModule rec {
   pname = "mdbook-sitemap-generator";
-  version = "1.1.0";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "loicsikidi";
     repo = "mdbook-sitemap-generator";
     tag = "v${version}";
-    hash = "sha256-BA6qfkdVxAq1XENZ1s2uCi2CGJ9FMmFNbF1YZvansiw=";
+    hash = "sha256-CGj6sWgOzI2cimX7KE0TkXOR6KlTOk2ZbIb3c5X6TSk=";
   };
 
-  vendorHash = "sha256-h5UXs7ujP0YIBKusstTDXvWBhqKpYs6HtzzdaB1+6Wg=";
+  vendorHash = "sha256-WUQW8EDJ7kT2CUZsNtlVUVwwqFRHkpkU6pFmx7/MDGg=";
 
 }
--- shell.nix
+++ shell.nix
@@ -1,20 +1,31 @@
 let
-  # mdbook pinned to 0.4.45
+  # mdbook pinned to 0.4.50
   # go to https://www.nixhub.io/packages/mdbook to the list of available versions
   nixpkgs = fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/dad564433178067be1fbdfcce23b546254b6d641.tar.gz";
+    "https://github.com/NixOS/nixpkgs/archive/bdac72d387dca7f836f6ef1fe547755fb0e9df61.tar.gz";
   pkgs = import nixpkgs {
     config = { };
     overlays = [ ];
   };
   pre-commit = import ./default.nix { };
-in pkgs.mkShellNoCC {
+  mdbook-sitemap-generator =
+    pkgs.callPackage ./nix/pkgs/mdbook-sitemap-generator.nix { };
+
+  # see https://github.com/NixOS/nixpkgs/issues/355486#issuecomment-2746187811
+  # to remember why we did this hack to fix the following error in github actions:
+  # '# runtime/cgo'
+  # 'Multiple conflicting values defined for DEVELOPER_DIR_arm64_apple_darwin
+  mkShellNoCC = pkgs.mkShellNoCC.override {
+    stdenv = pkgs.stdenvNoCC.override { extraBuildInputs = [ ]; };
+  };
+in mkShellNoCC {
   packages = with pkgs; [
     mdbook
     mdbook-linkcheck
+    mdbook-sitemap-generator
 
-    go # v1.23.5
-    delve # v1.24.0
+    go # v1.24.3
+    delve # v1.24.2
 
     # required to run TPM simulator
     # source: https://github.com/google/go-tpm-tools/tree/main/simulator
