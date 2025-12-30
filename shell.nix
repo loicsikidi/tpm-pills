498383196706a6c13fbab0ac2acb0a6deea1f920
loicsikidi <loic.sikidi@gmail.com>
2025-12-30 14:20:02 +0100

    chore: little improvements
    Notes:
      - migrate shell.nix to nix-shell-toolbox
      - fix github workflows security issues (ie. zizmor)
      - bump shell.nix to a newer version of nixpkgs to get mdbook v0.4.52
      - remove pre-commit from repo

--- shell.nix
+++ shell.nix
@@ -1,48 +1,65 @@
 let
-  # mdbook pinned to 0.4.50
+  # mdbook pinned to 0.4.52
   # go to https://www.nixhub.io/packages/mdbook to the list of available versions
-  nixpkgs = fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/bdac72d387dca7f836f6ef1fe547755fb0e9df61.tar.gz";
+  nixpkgs =
+    fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/ee09932cedcef15aaf476f9343d1dea2cb77e261.tar.gz";
   pkgs = import nixpkgs {
-    config = { };
-    overlays = [ ];
+    config = {};
+    overlays = [];
   };
-  pre-commit = import ./default.nix { };
   mdbook-sitemap-generator =
-    pkgs.callPackage ./nix/pkgs/mdbook-sitemap-generator.nix { };
+    pkgs.callPackage ./nix/pkgs/mdbook-sitemap-generator.nix {};
+
+  helpers = import (builtins.fetchTarball
+    "https://github.com/loicsikidi/nix-shell-toolbox/tarball/main") {
+    inherit pkgs;
+    hooksConfig = {
+      gotest.settings.flags = "-race";
+      lychee.enable = false;
+    };
+  };
 
   # see https://github.com/NixOS/nixpkgs/issues/355486#issuecomment-2746187811
   # to remember why we did this hack to fix the following error in github actions:
   # '# runtime/cgo'
   # 'Multiple conflicting values defined for DEVELOPER_DIR_arm64_apple_darwin
   mkShellNoCC = pkgs.mkShellNoCC.override {
-    stdenv = pkgs.stdenvNoCC.override { extraBuildInputs = [ ]; };
+    stdenv = pkgs.stdenvNoCC.override {extraBuildInputs = [];};
   };
-in mkShellNoCC {
-  packages = with pkgs; [
-    mdbook
-    mdbook-linkcheck
-    mdbook-sitemap-generator
+in
+  mkShellNoCC {
+    packages = with pkgs;
+      [
+        mdbook
+        mdbook-linkcheck
+        mdbook-sitemap-generator
 
-    go # v1.24.3
-    delve # v1.24.2
+        # required to run TPM simulator
+        # source: https://github.com/google/go-tpm-tools/tree/main/simulator
+        gcc
+        openssl
 
-    # required to run TPM simulator
-    # source: https://github.com/google/go-tpm-tools/tree/main/simulator
-    gcc
-    openssl
+        swtpm
 
-    swtpm
+        # we install tpm2-tools only in supported platforms (i.e. linux)
+        (lib.optional (lib.elem stdenv.system tpm2-tools.meta.platforms) tpm2-tools)
+      ]
+      ++ helpers.packages;
+    # we disable the hardening due to this error: https://github.com/tpm2-software/tpm2-tools/issues/1561
+    # fix found here: https://github.com/NixOS/nixpkgs/issues/18995#issuecomment-249748307
+    hardeningDisable = ["fortify"];
 
-    # we install tpm2-tools only in supported platforms (i.e. linux)
-    (lib.optional (lib.elem stdenv.system tpm2-tools.meta.platforms) tpm2-tools)
-  ];
-  # we disable the hardening due to this error: https://github.com/tpm2-software/tpm2-tools/issues/1561
-  # fix found here: https://github.com/NixOS/nixpkgs/issues/18995#issuecomment-249748307
-  hardeningDisable = [ "fortify" ];
+    shellHook = ''
+      ${helpers.shellHook}
+      echo "Development environment ready!"
+      echo "  - Go version: $(go version)"
+    '';
 
-  shellHook = ''
-    ${pre-commit.pre-commit-check.shellHook}
-  '';
-  buildInputs = pre-commit.pre-commit-check.enabledPackages;
-}
+    env = {
+      CGO_ENABLED = "1";
+
+      # Disable warnings from TPM simulator C code
+      CGO_CFLAGS = "-Wno-array-bounds -Wno-stringop-overflow";
+    };
+  }
