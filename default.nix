f38681308d852579b4c61f8ebcf83f8feb3c62c6
Loïc Sikidi <11026721+loicsikidi@users.noreply.github.com>
2025-12-30 17:43:47 +0100

    ci: migrate to new baseline (#48)

--- default.nix
+++ default.nix
@@ -1,33 +1,33 @@
-{ pkgs ? import <nixpkgs> { }, }:
-let
+{pkgs ? import <nixpkgs> {}}: let
   mdbook-sitemap-generator =
-    pkgs.callPackage ./nix/pkgs/mdbook-sitemap-generator.nix { };
-in with pkgs; {
-  html-split = stdenvNoCC.mkDerivation {
-    name = "tpm-pills";
-    src = lib.cleanSource ./.;
+    pkgs.callPackage ./nix/pkgs/mdbook-sitemap-generator.nix {};
+in
+  with pkgs; {
+    html-split = stdenvNoCC.mkDerivation {
+      name = "tpm-pills";
+      src = lib.cleanSource ./.;
 
-    nativeBuildInputs = [ mdbook mdbook-linkcheck mdbook-sitemap-generator ];
+      nativeBuildInputs = [mdbook mdbook-linkcheck mdbook-sitemap-generator];
 
-    buildPhase = ''
-      runHook preBuild
+      buildPhase = ''
+        runHook preBuild
 
-      # We can't check external links inside the sandbox, but it's good to check them outside the sandbox.
-      substituteInPlace book.toml --replace-fail 'follow-web-links = true' 'follow-web-links = false'
-      mdbook build
+        # We can't check external links inside the sandbox, but it's good to check them outside the sandbox.
+        substituteInPlace book.toml --replace-fail 'follow-web-links = true' 'follow-web-links = false'
+        mdbook build
 
-      runHook postBuild
-    '';
+        runHook postBuild
+      '';
 
-    installPhase = ''
-      runHook preInstall
+      installPhase = ''
+        runHook preInstall
 
-      dst=$out/tpm-pills
-      mkdir -p "$dst"
-      mv book/html/* "$dst"/
-      mv book/sitemap-generator/* "$dst"/
+        dst=$out/tpm-pills
+        mkdir -p "$dst"
+        mv book/html/* "$dst"/
+        mv book/sitemap-generator/* "$dst"/
 
-      runHook postInstall
-    '';
-  };
-}
+        runHook postInstall
+      '';
+    };
+  }
